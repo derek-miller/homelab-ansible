02b64fa65b81baf84ccdfc0707a8919707b696d5
Derek Miller <demiller@netflix.com>
2021-02-08 15:35:19 -0500

    * Added support for sending logs to influx using rsyslog * Added profitability command to the nicehash-stats script * Refactored the tplink-plug-stats script to use the python-kasa library * Tweaked the fan setting for the GTX 1070 overclock

--- roles/mining/nicehash-stats/files/nicehash-stats.py
+++ roles/mining/nicehash-stats/files/nicehash-stats.py
@@ -1,23 +1,34 @@
-#!/usr/bin/env python3
 import datetime
 import hashlib
 import hmac
 import json
+import locale
+import re
 import sys
 import time
+import traceback
 import uuid
 
 import click
 import requests
+from tabulate import tabulate, tabulate_formats
+
+locale.setlocale(locale.LC_ALL, "")
 
 CHARSET = "ISO-8859-1"
 NICEHASH_HOST = "api2.nicehash.com"
 
 
-def nicehash_request(api_key, api_secret, org_id, method, path, auth=True, **kwargs):
-    if auth:
+def nicehash_request(
+    method, path, api_key=None, api_secret=None, org_id=None, **kwargs
+):
+    if api_key and api_secret and org_id:
         time_ms = str(
-            int(round(datetime.datetime.now(tz=datetime.timezone.utc).timestamp() * 1000))
+            int(
+                round(
+                    datetime.datetime.now(tz=datetime.timezone.utc).timestamp() * 1000
+                )
+            )
         )
         nonce = str(uuid.uuid4())
 
@@ -56,19 +67,26 @@ def nicehash_request(api_key, api_secret, org_id, method, path, auth=True, **kwa
         )
     kwargs.setdefault("headers", {}).update({"X-Request-Id": str(uuid.uuid4())})
     return requests.request(
-        method, f"https://{NICEHASH_HOST.rstrip('/')}/{(path or '').lstrip('/')}", **kwargs
+        method,
+        f"https://{NICEHASH_HOST.rstrip('/')}/{(path or '').lstrip('/')}",
+        **kwargs,
     )
 
 
 def get_nicehash_algorithm_stats(api_key, api_secret, org_id):
     response = nicehash_request(
-        api_key, api_secret, org_id, "get", "/main/api/v2/mining/algorithms"
+        "get",
+        "/main/api/v2/mining/algorithms",
+        api_key=api_key,
+        api_secret=api_secret,
+        org_id=org_id,
     )
     response.raise_for_status()
     algorithms = {a["order"]: a for a in response.json().get("miningAlgorithms", {})}
 
     response = nicehash_request(
-        api_key, api_secret, org_id, "get", "/main/api/v2/public/stats/global/current", auth=False
+        "get",
+        "/main/api/v2/public/stats/global/current",
     )
     response.raise_for_status()
     for algorithm_stats in response.json().get("algos", {}):
@@ -80,7 +98,11 @@ def get_nicehash_algorithm_stats(api_key, api_secret, org_id):
 
 def get_nicehash_exchange_rates(api_key, api_secret, org_id):
     response = nicehash_request(
-        api_key, api_secret, org_id, "get", "/main/api/v2/exchangeRate/list"
+        "get",
+        "/main/api/v2/exchangeRate/list",
+        api_key=api_key,
+        api_secret=api_secret,
+        org_id=org_id,
     )
     response.raise_for_status()
     return {
@@ -91,7 +113,14 @@ def get_nicehash_exchange_rates(api_key, api_secret, org_id):
 
 def to_line_protocol(measurement_name, tags, fields, ts=None):
     tags = ",".join(
-        "{}={}".format(tag, tag_value) for tag, tag_value in tags.items() if tag_value is not None
+        "{}={}".format(
+            tag,
+            re.sub(r"\s+", r"\ ", tag_value.strip())
+            if isinstance(tag_value, str)
+            else tag_value,
+        )
+        for tag, tag_value in tags.items()
+        if tag_value is not None
     )
     fields = ",".join(
         "{}={}".format(field, field_value)
@@ -120,11 +149,11 @@ def cli(ctx, api_key, api_secret, org_id):
 def rig(ctx, rig_id):
     try:
         response = nicehash_request(
-            ctx.obj["api_key"],
-            ctx.obj["api_secret"],
-            ctx.obj["org_id"],
             "get",
-            "/main/api/v2/mining/rigs2/",
+            f"/main/api/v2/mining/rigs2/",
+            api_key=ctx.obj["api_key"],
+            api_secret=ctx.obj["api_secret"],
+            org_id=ctx.obj["org_id"],
         )
         response.raise_for_status()
         data = response.json()
@@ -169,10 +198,19 @@ def rig(ctx, rig_id):
 
                 speed_accepted = float(rig_stat.get("speedAccepted", 0))
                 pricing = float(algorithm_stats.get(algorithm, {}).get("pricing", 0))
-                market_factor = float(algorithm_stats.get(algorithm, {}).get("marketFactor", 0))
-                mining_factor = float(algorithm_stats.get(algorithm, {}).get("miningFactor", 0))
+                market_factor = float(
+                    algorithm_stats.get(algorithm, {}).get("marketFactor", 0)
+                )
+                mining_factor = float(
+                    algorithm_stats.get(algorithm, {}).get("miningFactor", 0)
+                )
                 bitcoin_per_day = (
-                    speed_accepted * mining_factor / market_factor * 1e-8 * pricing * market_factor
+                    speed_accepted
+                    * mining_factor
+                    / market_factor
+                    * 1e-8
+                    * pricing
+                    * market_factor
                 )
 
                 click.echo(
@@ -196,14 +234,24 @@ def rig(ctx, rig_id):
                             "difficulty": float(rig_stat.get("difficulty", 0)),
                             "uptime_ms": float(rig_stat.get("timeConnected", 0)),
                             "accepted": speed_accepted,
-                            "rejected_target": float(rig_stat.get("speedRejectedR1Target", 0)),
-                            "rejected_stale": float(rig_stat.get("speedRejectedR2Stale", 0)),
+                            "rejected_target": float(
+                                rig_stat.get("speedRejectedR1Target", 0)
+                            ),
+                            "rejected_stale": float(
+                                rig_stat.get("speedRejectedR2Stale", 0)
+                            ),
                             "rejected_duplicate": float(
                                 rig_stat.get("speedRejectedR3Duplicate", 0)
                             ),
-                            "rejected_time": float(rig_stat.get("speedRejectedR4NTime", 0)),
-                            "rejected_other": float(rig_stat.get("speedRejectedR5Other", 0)),
-                            "rejected_total": float(rig_stat.get("speedRejectedTotal", 0)),
+                            "rejected_time": float(
+                                rig_stat.get("speedRejectedR4NTime", 0)
+                            ),
+                            "rejected_other": float(
+                                rig_stat.get("speedRejectedR5Other", 0)
+                            ),
+                            "rejected_total": float(
+                                rig_stat.get("speedRejectedTotal", 0)
+                            ),
                         },
                         ts=float(rig_stat.get("statsTime", 0)) / 1000,
                     ),
@@ -219,11 +267,11 @@ def rig(ctx, rig_id):
 def wallet(ctx):
     try:
         response = nicehash_request(
-            ctx.obj["api_key"],
-            ctx.obj["api_secret"],
-            ctx.obj["org_id"],
             "get",
             "/main/api/v2/accounting/accounts2",
+            api_key=ctx.obj["api_key"],
+            api_secret=ctx.obj["api_secret"],
+            org_id=ctx.obj["org_id"],
         )
         response.raise_for_status()
         data = response.json()
@@ -236,9 +284,7 @@ def wallet(ctx):
             click.echo(
                 to_line_protocol(
                     measurement_name="nicehash.wallet_total",
-                    tags={
-                        "currency": total["currency"]
-                    },
+                    tags={"currency": total["currency"]},
                     fields={
                         "exchange_rate": exchange_rates[(total["currency"], "USD")],
                         "balance": float(total["totalBalance"]),
@@ -250,19 +296,22 @@ def wallet(ctx):
             )
 
         for account in data.get("currencies") or []:
+            exchange_rate_key = (account["currency"], "USD")
+            if exchange_rate_key not in exchange_rates:
+                continue
             click.echo(
                 to_line_protocol(
                     measurement_name="nicehash.wallet",
                     tags={"currency": account["currency"]},
                     fields={
-                        "exchange_rate": exchange_rates[(account["currency"], "USD")],
+                        "exchange_rate": exchange_rates[exchange_rate_key],
                         "balance": float(account["totalBalance"]),
                     },
                 ),
                 file=sys.stdout,
             )
-    except Exception as e:
-        click.echo(str(e), err=True)
+    except Exception:
+        click.echo(traceback.format_exc(), err=True)
         ctx.exit(1)
 
 
@@ -270,11 +319,11 @@ def wallet(ctx):
 @click.pass_context
 def market(ctx):
     response = nicehash_request(
-        ctx.obj["api_key"],
-        ctx.obj["api_secret"],
-        ctx.obj["org_id"],
         "get",
         "/exchange/api/v2/info/marketStats",
+        api_key=ctx.obj["api_key"],
+        api_secret=ctx.obj["api_secret"],
+        org_id=ctx.obj["org_id"],
     )
     response.raise_for_status()
     for market_symbol, stats in response.json().items():
@@ -301,6 +350,208 @@ def market(ctx):
             ),
             file=sys.stdout,
         )
+
+
+class ClickNicehashAlgorithmChoice(click.Choice):
+    name = "nicehash-algorithm-choice"
+
+    def __init__(self):
+        super().__init__([], case_sensitive=False)
+        self._known_algorithms = None
+
+    def convert(self, value, param, ctx):
+        if not self.choices:
+            response = nicehash_request(
+                "get",
+                "/main/api/v2/mining/algorithms",
+            )
+            response.raise_for_status()
+            self.choices = sorted(
+                {a["algorithm"] for a in response.json()["miningAlgorithms"]}
+            )
+        return super().convert(value, param, ctx)
+
+
+class ClickRegex(click.ParamType):
+    name = "regex"
+
+    def __init__(self, as_bytes: bool = False, flags: int = 0):
+        self._as_bytes = as_bytes
+        self._flags = flags
+
+    def convert(self, value, param, ctx):
+        try:
+            return re.compile(
+                value.encode("utf-8") if self._as_bytes else value, flags=self._flags
+            )
+        except re.error as e:
+            self.fail(str(e), param, ctx)
+
+
+@cli.command()
+@click.option(
+    "-c",
+    "--category",
+    "categories",
+    type=click.Choice(["GPU", "CPU", "ASIC"], case_sensitive=False),
+    multiple=True,
+)
+@click.option(
+    "-a",
+    "--algorithm",
+    "algorithms",
+    type=ClickNicehashAlgorithmChoice(),
+    multiple=True,
+)
+@click.option(
+    "-d",
+    "--device-regex",
+    type=ClickRegex(flags=re.IGNORECASE),
+    help="Index only resource events whose key matches this regex (ex: 'workflow:dev:dx.test.nop')",
+)
+@click.option("-k", "--kwh-cost", default=0.2, type=click.FLOAT)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(
+        exists=False, dir_okay=False, file_okay=True, allow_dash=True, writable=True
+    ),
+    default="-",
+)
+@click.option(
+    "-f",
+    "--output-format",
+    type=click.Choice(["csv"] + tabulate_formats),
+    default="plain",
+)
+@click.pass_context
+def profitability(
+    ctx, categories, algorithms, device_regex, kwh_cost, output, output_format
+):
+    bitcoin_value = get_nicehash_exchange_rates(
+        ctx.obj["api_key"], ctx.obj["api_secret"], ctx.obj["org_id"]
+    )[("BTC", "USD")]
+
+    response = nicehash_request(
+        "get",
+        "/main/api/v2/public/profcalc/devices",
+    )
+    response.raise_for_status()
+
+    one_day_ago = datetime.datetime.now() - datetime.timedelta(days=1)
+    device_profitability = []
+
+    for device_short in (response.json() or {}).get("devicesShort") or []:
+        if (
+            not device_short.get("name")
+            or (categories and device_short["category"] not in categories)
+            or (device_regex and not device_regex.match(device_short["name"]))
+        ):
+            continue
+        response = nicehash_request(
+            "get",
+            "/main/api/v2/public/profcalc/device",
+            params={"device": device_short["id"]},
+        )
+        response.raise_for_status()
+
+        device = response.json()
+        speeds = {
+            a: s
+            for a, s in json.loads(device["speeds"]).items()
+            # Filter out zero/empty speeds and algorithms not asked for
+            if s and float(s) and (not algorithms or a in algorithms)
+        }
+        for algorithm_name, speed in speeds.items():
+            response = nicehash_request(
+                "post",
+                "/main/api/v2/public/profcalc/profitability",
+                json={"speeds": {algorithm_name: speed}},
+            )
+            response.raise_for_status()
+
+            algorithm_profitability = response.json()
+            count = 0
+            total = 0.0
+            algorithm_code = algorithm_profitability["codes"][algorithm_name]
+            for ts, p in sorted(
+                (algorithm_profitability.get("values") or {}).items(),
+                key=lambda item: item[0],
+                reverse=True,
+            ):
+                if datetime.datetime.fromtimestamp(int(ts)) < one_day_ago:
+                    break
+                assert p["a"] == algorithm_code
+                count += 1
+                total += p["p"]
+
+            if count == 0 or total == 0:
+                continue
+            bitcoin_per_day = total / count
+            revenue_per_day = bitcoin_per_day * bitcoin_value
+            cost_per_day = kwh_cost * (device["power"] / 1000.0 * 24)
+            device_profitability.append(
+                {
+                    "device": device["name"],
+                    "algorithm_name": algorithm_name,
+                    "speed": speed,
+                    "power_w": device["power"],
+                    "kwh_cost": kwh_cost,
+                    "power_cost_per_day": locale.currency(cost_per_day),
+                    "bitcoin_per_day": bitcoin_per_day,
+                    "revenue_per_day": locale.currency(revenue_per_day),
+                    "profit_per_day": locale.currency(revenue_per_day - cost_per_day),
+                    "bitcoin_value": locale.currency(bitcoin_value),
+                    "profitability": round(
+                        (revenue_per_day / cost_per_day) * 100, ndigits=2
+                    ),
+                }
+            )
+    # Sort by profitability
+    device_profitability.sort(key=lambda p: p["profitability"], reverse=True)
+
+    with sys.stdout if output == "-" else open(output, "w+") as f:
+        headers = [
+            "GPU",
+            "Algorithm",
+            "Speed",
+            "Power Consumption (W)",
+            "kWh Cost",
+            "Power Cost / 24h",
+            "BTC / 24h",
+            "BTC Price",
+            "Revenue / 24h",
+            "Profit / 24h",
+            "Profitability %",
+        ]
+        rows = [
+            [
+                str(x)
+                for x in [
+                    dp["device"],
+                    dp["algorithm_name"],
+                    dp["speed"],
+                    dp["power_w"],
+                    dp["kwh_cost"],
+                    dp["power_cost_per_day"],
+                    dp["bitcoin_per_day"],
+                    dp["bitcoin_value"],
+                    dp["revenue_per_day"],
+                    dp["profit_per_day"],
+                    dp["profitability"],
+                ]
+            ]
+            for dp in device_profitability
+        ]
+        if output_format == "csv":
+            click.echo(",".join(headers), file=f)
+            for row in rows:
+                click.echo(",".join(row), file=f)
+        else:
+            click.echo(
+                tabulate(rows, headers=headers, tablefmt=output_format),
+                file=f,
+            )
 
 
 if __name__ == "__main__":
